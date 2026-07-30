c581986aa4b230b7d0dda3c7b8395a98e87fb17e
B3NA-LAPTOP\obena <obenavides06@unitec.edu>
2026-07-29 19:13:20 -0600

    -creando componente customButton -implementando componente personalizado customButton en App.tsx

--- BookApp/App.tsx
+++ BookApp/App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button} from 'react-native';
+import CustomButton from './src/components/CustomButton';
 //import { Button } from 'react-native/types_generated/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Button
-        title="Soy un boton"
-        onPress={()=>{}}
-      />
+      <CustomButton/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ BookApp/src/components/CustomButton.tsx
@@ -0,0 +1,16 @@
+
+// myCustomButton = camel case
+// MyCustomButton = pascal case
+// my_custom_button = snake case
+
+import { Button, Text, View } from "react-native";
+
+export default function CustomButton () {
+    
+    return(
+        <View>
+            <Text>Titulo cualquier de Boton</Text>
+            <Button title="4"/>
+        </View>
+    );
+}
